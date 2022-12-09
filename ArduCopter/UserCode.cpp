b9f0dac3a640735077563230becaf6929911fbf0
ashutoshsoni7523 <ashutoshsoni7523@gmail.com>
2022-12-09 20:05:03 +0530

    09/12/2022

--- ArduCopter/UserCode.cpp
+++ ArduCopter/UserCode.cpp
@@ -2,9 +2,40 @@
 
 #ifdef USERHOOK_INIT
 void Copter::userhook_init()
-{
-    // put your initialisation code here
+{   
+     // put your initialisation code here
     // this will be called once at start-up
+    bool ModeHeadingLock::init(bool ignore_checks)
+{
+    if (!copter.failsafe.radio)
+    {
+        float target_roll, target_pitch;
+        // apply SIMPLE mode transform to pilot inputs
+        update_simple_mode();
+
+        // convert pilot input to lean angles
+        get_pilot_desired_lean_angles(target_roll, target_pitch, loiter_nav->get_angle_max_cd(), attitude_control->get_althold_lean_angle_max_cd());
+
+        // process pilot's roll and pitch input
+        loiter_nav->set_pilot_desired_acceleration(target_roll, target_pitch);
+    }
+    else
+    {
+        // clear out pilot desired acceleration in case radio failsafe event occurs and we do not switch to RTL for some reason
+        loiter_nav->clear_pilot_desired_acceleration();
+    }
+    loiter_nav->init_target();
+
+    // initialise the vertical position controller
+    if (!pos_control->is_active_z())
+    {
+        pos_control->init_z_controller();
+    }
+
+    // set vertical speed and acceleration limits
+    pos_control->set_max_speed_accel_z(-get_pilot_speed_dn(), g.pilot_speed_up, g.pilot_accel_z);
+    pos_control->set_correction_speed_accel_z(-get_pilot_speed_dn(), g.pilot_speed_up, g.pilot_accel_z);
+}
 }
 #endif
 
@@ -12,6 +43,96 @@ void Copter::userhook_init()
 void Copter::userhook_FastLoop()
 {
     // put your 100Hz code here
+    #if MODE_HEADING_LOCK == ENABLED
+    bool ModeHeadingLock::run()
+{
+    float target_roll, target_pitch;
+    float target_yaw_rate = 0.0f;
+    float target_climb_rate = 0.0f;
+
+    // set vertical speed and acceleration limits
+    pos_control->set_max_speed_accel_z(-get_pilot_speed_dn(), g.pilot_speed_up, g.pilot_accel_z);
+
+    // process pilot inputs unless we are in radio failsafe
+    if (!copter.failsafe.radio)
+    {
+        // apply SIMPLE mode transform to pilot inputs
+        update_simple_mode();
+
+        // convert pilot input to lean angles
+        get_pilot_desired_lean_angles(target_roll, target_pitch, loiter_nav->get_angle_max_cd(), attitude_control->get_althold_lean_angle_max_cd());
+
+        // process pilot's roll and pitch input
+        loiter_nav->set_pilot_desired_acceleration(target_roll, target_pitch);
+
+        // get pilot's desired yaw rate
+        // target_yaw_rate = get_pilot_desired_yaw_rate(channel_yaw->norm_input_dz());   // get pilot desired climb rate
+
+        auto_yaw.set_mode(AUTO_YAW_RESETTOARMEDYAW); // point towards heading at time motors were armed
+
+        target_climb_rate = get_pilot_desired_climb_rate(channel_throttle->get_control_in());
+        target_climb_rate = constrain_float(target_climb_rate, -get_pilot_speed_dn(), g.pilot_speed_up);
+    }
+    else
+    {
+        // clear out pilot desired acceleration in case radio failsafe event occurs and we do not switch to RTL for some reason
+        loiter_nav->clear_pilot_desired_acceleration();
+    }
+
+    // relax loiter target if we might be landed
+    if (copter.ap.land_complete_maybe)
+    {
+        loiter_nav->soften_for_landing();
+    }
+
+    // Loiter State Machine Determination
+    AltHoldModeState loiter_state = get_alt_hold_state(target_climb_rate);
+
+    // Loiter State Machine
+    switch (loiter_state)
+    {
+
+    case AltHold_Takeoff:
+        // initiate take-off
+        if (!takeoff.running())
+        {
+            takeoff.start(constrain_float(g.pilot_takeoff_alt, 0.0f, 1000.0f));
+        }
+
+        // get avoidance adjusted climb rate
+        target_climb_rate = get_avoidance_adjusted_climbrate(target_climb_rate);
+
+        // set position controller targets adjusted for pilot input
+        takeoff.do_pilot_takeoff(target_climb_rate);
+
+        // run loiter controller
+        loiter_nav->update();
+
+        // call attitude controller
+        attitude_control->input_thrust_vector_rate_heading(loiter_nav->get_thrust_vector(), target_yaw_rate, false);
+        break;
+
+    case AltHold_Flying:
+        // set motors to full range
+        motors->set_desired_spool_state(AP_Motors::DesiredSpoolState::THROTTLE_UNLIMITED);
+        // call attitude controller
+        attitude_control->input_thrust_vector_rate_heading(loiter_nav->get_thrust_vector(), target_yaw_rate, false);
+
+        // get avoidance adjusted climb rate
+        target_climb_rate = get_avoidance_adjusted_climbrate(target_climb_rate);
+
+        // update the vertical offset based on the surface measurement
+        copter.surface_tracking.update_surface_offset();
+
+        // Send the commanded climb rate to the position controller
+        pos_control->set_pos_target_z_from_climb_rate_cm(target_climb_rate);
+        break;
+    }
+
+    // run the vertical position controller and set output throttle
+    pos_control->update_z_controller();
+}
+#endif
 }
 #endif
 
--- ArduCopter/mode.h
+++ ArduCopter/mode.h
@@ -39,6 +39,7 @@ public:
         AUTOROTATE =   26,  // Autonomous autorotation
         AUTO_RTL =     27,  // Auto RTL, this is not a true mode, AUTO will report as this mode if entered to perform a DO_LAND_START Landing sequence
         TURTLE =       28,  // Flip over after crash
+        HEADING_LOCK = 29,  // Keeps at current heading, takes pitch and roll
 
         // Mode number 127 reserved for the "drone show mode" in the Skybrush
         // fork at https://github.com/skybrush-io/ardupilot
@@ -1167,6 +1168,7 @@ private:
 #endif
 
 };
+
 
 
 class ModePosHold : public Mode {
